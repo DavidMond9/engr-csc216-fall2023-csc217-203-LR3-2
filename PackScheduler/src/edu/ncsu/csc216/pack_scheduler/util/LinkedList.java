bd232932092f5f45ed315495099cd14cd9f4c004
Joey Hughes <jehughe3@ncsu.edu>
2023-11-13 16:25:19 -0500

    [Implementation/Test] (Pair Prog with David Mond) Implemented LinkedList

---
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/util/LinkedList.java
@@ -0,0 +1,282 @@
+package edu.ncsu.csc216.pack_scheduler.util;
+
+import java.util.AbstractSequentialList;
+import java.util.ListIterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Linked List based on AbstractSequentialList that utilizes an Iterator.
+ * @param <E> The type to store in the list.
+ * @author Joey Hughes, David Mond
+ */
+public class LinkedList<E> extends AbstractSequentialList<E> {
+
+	/** The front node of the list. */
+	private ListNode front;
+	
+	/** The back node of the list. */
+	private ListNode back;
+	
+	/** The size of the list. */
+	private int size;
+	
+	/** 
+	 * Constructs a new LinkedList with 0 size and no elements.
+	 */
+	public LinkedList() {
+		front = new ListNode(null);
+		back = new ListNode(null);
+		front.next = back;
+		back.prev = front;
+		size = 0;
+	}
+	
+	/**
+	 * Returns size of list.
+	 * @return returns size of list.
+	 */
+	@Override
+	public int size() {
+		return size;
+	}
+
+	/**
+	 * Adds the given element to the given index. If it is a duplicate element or is null data,
+	 *     an IllegalArgumentException is thrown.
+	 * @param index The index to add the element to.
+	 * @param element The data of the new element.
+	 * @throws IllegalArgumentException If the element is a duplicate of another element in the list.
+	 */
+	@Override
+	public void add(int index, E element) {
+		if(this.contains(element)) {
+			throw new IllegalArgumentException("Cannot add duplicate element.");
+		}
+		super.add(index, element);
+	}
+	
+	/**
+	 * Replaces the element at the given index to the given data.
+	 * @param index The index to replace the data for.
+	 * @param element The element to replace with.
+	 */
+	@Override
+	public E set(int index, E element) {
+		if(this.contains(element)) {
+			throw new IllegalArgumentException("Cannot add duplicate element.");
+		}
+		return super.set(index, element);
+	}
+
+	/**
+	 * Returns a new ListIterator.
+	 * @param index integer to represent the index.
+	 * @return returns a new ListIterator.
+	 */
+	@Override
+	public ListIterator<E> listIterator(int index) {
+		return new LinkedListIterator(index);
+	}
+	
+	
+	/**
+	 * Class to represent a node in the list
+	 * @author Joey Hughes, David Mond
+	 */
+	private class ListNode {
+		
+		/** The data at this node in the list. */
+		public E data;
+		
+		/** The next node in the list. */
+		public ListNode next;
+		
+		/** The previous node in the list. */
+		public ListNode prev;
+		
+		/**
+		 * Create a list node with null next and previous parameters.
+		 * @param data The data to put in the node.
+		 */
+		public ListNode(E data) {
+			this.data = data;
+		}
+		
+		/**
+		 * Create a list node with the given data and previous and next references.
+		 * @param data The data to hold in this node.
+		 * @param next The next node.
+		 * @param prev The previous node.
+		 */
+		public ListNode(E data, ListNode next, ListNode prev) {
+			this.data = data;
+			this.next = next;
+			this.prev = prev;
+		}
+	}
+	
+	/**
+	 * The iterator for this LinkedList.
+	 * @author Joey Hughes, David Mond
+	 */
+	private class LinkedListIterator implements ListIterator<E> {
+		
+		/** The previous node in the list. */
+		private ListNode previous;
+		
+		/** The next node in the list. */
+		private ListNode next;
+		
+		/** The index of the previous node in the list. */
+		private int previousIndex;
+		
+		/** The index of the next node in the list. */
+		private int nextIndex;
+		
+		/** The node that was last retrieved by this iterator. */
+		private ListNode lastRetrieved;
+
+		/**
+		 * Creates a new LinkedListIterator.
+		 * @param index Index where the LinkedListIterator is.
+		 */
+		public LinkedListIterator(int index) {
+			if(index > size || index < 0) {
+				throw new IndexOutOfBoundsException("Index out of bounds.");
+			}
+			ListNode curr = front;
+			 for(int i = 0; i < index; i++) {
+				 curr = curr.next;
+			 }
+			 this.previous = curr;
+			 this.next = curr.next;
+			 this.previousIndex = index - 1;
+			 this.nextIndex = index;
+			 this.lastRetrieved = null;
+		}
+		
+		/**
+		 * Returns true if next node, returns false if not.
+		 * @return Returns a boolean if next or not.
+		 */
+		@Override
+		public boolean hasNext() {
+			if(next.data != null) {
+				return true;
+			}
+			return false;
+		}
+		/**
+		 * Returns the data in the next ListNode.
+		 * @return Returns E for the data in the next ListNode.
+		 */
+		@Override
+		public E next() {
+			if(!hasNext()) {
+				throw new NoSuchElementException("No such element.");
+			}
+			E nextData = next.data;
+			this.lastRetrieved = next;
+			this.previous = next;
+			this.next = next.next;
+			this.previousIndex++;
+			this.nextIndex++;
+			return nextData;
+		}
+		/**
+		 * Returns true if previous node, returns false if not.
+		 * @return Returns a boolean if previous or not.
+		 */
+		@Override
+		public boolean hasPrevious() {
+			if(previous.data != null) {
+				return true;
+			}
+			return false;
+		}
+		/**
+		 * Returns the data in the previous ListNode.
+		 * @return Returns E for the data in the previous ListNode.
+		 */
+		@Override
+		public E previous() {
+			if(!hasPrevious()) {
+				throw new NoSuchElementException("No such element.");
+			}
+			E prevData = previous.data;
+			this.lastRetrieved = previous;
+			this.next = previous;
+			this.previous = previous.prev;
+			this.previousIndex--;
+			this.nextIndex--;
+			return prevData;
+		}
+		/**
+		 * Returns the nextIndex field.
+		 * @return Returns an integer for the nextIndex.
+		 */
+		@Override
+		public int nextIndex() {
+			return this.nextIndex;
+		}
+		/**
+		 * Returns the previousIndex field.
+		 * @return Returns an integer for the previousIndex.
+		 */
+		@Override
+		public int previousIndex() {
+			return this.previousIndex;
+		}
+
+		
+		@Override
+		public void remove() {
+			if(lastRetrieved == null) throw new IllegalStateException("No Last Retrieved element");
+			if(lastRetrieved == previous) {
+				previous = lastRetrieved.prev;
+				previous.next = next;
+			} else if(lastRetrieved == this.next) {
+				next = lastRetrieved.next;
+				next.prev = previous;
+			}
+			size--;
+		}
+
+		/**
+		 * Sets the last retrieved element's data to the given data.
+		 * @param e The data to replace the element with.
+		 * @throws IllegalStateException if there is no last retrieved element.
+		 * @throws NullPointerException if the given data is null
+		 */
+		@Override
+		public void set(E e) {
+			if(lastRetrieved == null) throw new IllegalStateException("No Last Retrieved element");
+			if(e == null) throw new NullPointerException("Cannot add null element.");
+			lastRetrieved.data = e;
+		}
+
+		/**
+		 * Adds a new ListNode to the list at the position of the iterator between this
+		 *     iterator's next and previous elements. After the addition, the next field
+		 *     stays the same, previous become the new element. LastRetrieved is set to null.
+		 * @param e The data to give the new node.
+		 * @throws NullPointerException if the given data is null.
+		 */
+		@Override
+		public void add(E e) {
+			if(e == null) {
+				throw new NullPointerException("Cannot add null objects."); 
+			}
+			ListNode curr = new ListNode(e, next, previous);
+			previous.next = curr;
+			next.prev = curr;
+			nextIndex++;
+			previous = previous.next;
+			previousIndex++;
+			this.lastRetrieved = null;
+			size++;
+		}
+	}
+
+
+}
---
+++ PackScheduler/test/edu/ncsu/csc216/pack_scheduler/util/LinkedListTest.java
@@ -0,0 +1,163 @@
+package edu.ncsu.csc216.pack_scheduler.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests the in house implementation of LinkedList
+ * @author Joey Hughes, David Mond
+ */
+class LinkedListTest {
+	/**
+	 * Tests the construction of an LinkedList
+	 */
+	@Test
+	void testLinkedList() {
+		assertDoesNotThrow(() -> {new LinkedList<String>(); });
+	}
+
+	
+	/**
+	 * Tests to see if add adds things to the list.
+	 * Checks to see if correct exceptions are thrown given incorrect arguments
+	 * Also tests to see if it adds them to the right places
+	 */
+	@Test
+	void testAddIntE() {
+		LinkedList<Integer> intList = new LinkedList<Integer>();
+		LinkedList<String> stringList = new LinkedList<String>();
+		assertDoesNotThrow(() -> intList.add(0, 15));
+		assertDoesNotThrow(() -> stringList.add(0, "Whatever"));
+		assertEquals(intList.get(0), 15);
+		assertEquals(stringList.get(0), "Whatever");
+		assertThrows(IndexOutOfBoundsException.class, 
+				() -> intList.add(-1, 10));
+		assertThrows(IndexOutOfBoundsException.class, 
+				() -> intList.add(5, 10));
+		assertThrows(IndexOutOfBoundsException.class, 
+				() -> intList.add(-1, 10));
+		assertThrows(NullPointerException.class, 
+				() -> intList.add(0, null));
+		assertThrows(IllegalArgumentException.class, 
+				() -> intList.add(0, 15));
+		assertDoesNotThrow(() -> intList.add(1, 5));
+		assertDoesNotThrow(() -> intList.add(1, 3));
+		assertDoesNotThrow(() -> intList.add(1, 7));
+		assertDoesNotThrow(() -> intList.add(1, 14));
+		assertDoesNotThrow(() -> intList.add(1, 13));
+		assertDoesNotThrow(() -> intList.add(1, 300));
+		assertDoesNotThrow(() -> intList.add(1, 400));
+		assertDoesNotThrow(() -> intList.add(1, 200));
+		assertDoesNotThrow(() -> intList.add(1, 301));
+		assertDoesNotThrow(() -> intList.add(1, 999));
+		assertAll(
+				() -> assertEquals(intList.get(0), 15),
+				() -> assertEquals(intList.get(1), 999),
+				() -> assertEquals(intList.get(2), 301),
+				() -> assertEquals(intList.get(3), 200),
+				() -> assertEquals(intList.get(4), 400),
+				() -> assertEquals(intList.get(5), 300),
+				() -> assertEquals(intList.get(6), 13),
+				() -> assertEquals(intList.get(7), 14),
+				() -> assertEquals(intList.get(8), 7),
+				() -> assertEquals(intList.get(9), 3),
+				() -> assertEquals(intList.get(10), 5)
+				);
+		
+	}
+
+	/**
+	 * Tests to see if removal of elements is correct
+	 * Also tests for correct thrown exceptions when giving invalid input
+	 * Also tests to see if remove correctly returns the removed element
+	 */
+	@Test
+	void testRemoveInt() {
+		LinkedList<Integer> intList = new LinkedList<Integer>();
+		LinkedList<String> stringList = new LinkedList<String>();
+		assertDoesNotThrow(() -> intList.add(0, 15));
+		assertDoesNotThrow(() -> stringList.add(0, "Whatever"));
+		assertEquals("Whatever", stringList.remove(0));
+		intList.add(1, 10);
+		intList.add(2, 11);
+		intList.add(3, 12);
+		intList.add(4, 13);
+		assertThrows(IndexOutOfBoundsException.class, 
+				() -> intList.remove(10));
+		assertThrows(IndexOutOfBoundsException.class, 
+				() -> intList.remove(-1));
+		assertEquals(10, intList.remove(1));
+		assertEquals(intList.get(1), 11);
+		assertEquals(13, intList.remove(3));
+		assertEquals(15, intList.remove(0));
+	}
+
+	/**
+	 * Tests to see if you can set an element in the list to something else
+	 * Also checks to see if what set returns is correct
+	 */
+	@Test
+	void testSetIntE() {
+		LinkedList<Integer> intList = new LinkedList<Integer>();
+		LinkedList<String> stringList = new LinkedList<String>();
+		assertDoesNotThrow(() -> intList.add(0, 15));
+		assertDoesNotThrow(() -> stringList.add(0, "Whatever"));
+		assertThrows(IllegalArgumentException.class, 
+				() -> intList.set(-20, 15));
+		assertThrows(IndexOutOfBoundsException.class, 
+				() -> stringList.set(500, "Now"));
+		assertThrows(NullPointerException.class, 
+				() -> stringList.set(0, null));
+		assertDoesNotThrow(() -> intList.add(1, 30));
+		assertDoesNotThrow(() -> intList.add(2, 45));
+		assertThrows(IllegalArgumentException.class, 
+				() -> intList.set(1, 30));
+		assertEquals(30, intList.set(1, 100));
+		assertEquals(3, intList.size());
+		assertEquals(100, intList.get(1));
+	}
+
+	/**
+	 * Tests to see if get returns the expected element
+	 */
+	@Test
+	void testGetInt() {
+		LinkedList<Integer> intList = new LinkedList<Integer>();
+		LinkedList<String> stringList = new LinkedList<String>();
+		assertDoesNotThrow(() -> intList.add(0, 15));
+		assertDoesNotThrow(() -> stringList.add(0, "Whatever"));
+		assertEquals(intList.get(0), 15);
+		assertEquals(stringList.get(0), "Whatever");
+		assertDoesNotThrow(() -> intList.add(1, 20));
+		assertDoesNotThrow(() -> stringList.add(1, "Whenever"));
+		assertEquals(intList.get(0), 15);
+		assertEquals(intList.get(1), 20);
+		assertEquals(stringList.get(0), "Whatever");
+		assertEquals(stringList.get(1), "Whenever");
+		assertThrows(IndexOutOfBoundsException.class, 
+				() -> intList.get(-500));
+		assertThrows(IndexOutOfBoundsException.class, 
+				() -> intList.get(500));
+	}
+	
+
+	/**
+	 * Tests to see whether returned size is correct
+	 */
+	@Test
+	void testSize() {
+		LinkedList<Integer> intList = new LinkedList<Integer>();
+		LinkedList<String> stringList = new LinkedList<String>();
+		assertEquals(0, intList.size());
+		intList.add(0, 1);
+		stringList.add(0, "Whatever");
+		assertEquals(1, intList.size());
+		intList.add(1, 2);
+		intList.add(2, 3);
+		assertEquals(3, intList.size());
+		intList.remove(0);
+		assertEquals(2, intList.size());
+	}
+
+}
