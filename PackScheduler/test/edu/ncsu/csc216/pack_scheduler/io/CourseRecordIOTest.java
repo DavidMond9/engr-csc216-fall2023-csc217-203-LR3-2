8b18eae63e3d8dded5e4a0742b64d5b48d847f13
Warren Long <wclong4@ncsu.edu>
2023-11-18 17:41:49 -0500

    Got rid of checkstyle error

--- PackScheduler/test/edu/ncsu/csc216/pack_scheduler/io/CourseRecordIOTest.java
+++ PackScheduler/test/edu/ncsu/csc216/pack_scheduler/io/CourseRecordIOTest.java
@@ -15,11 +15,10 @@ import java.util.Scanner;
 
 
 import edu.ncsu.csc216.pack_scheduler.course.Course;
-import edu.ncsu.csc216.pack_scheduler.course.validator.InvalidTransitionException;
 import edu.ncsu.csc217.collections.list.SortedList;
 
 /**
- * Tests CouresRecordIO.
+ * Tests CourseRecordIO.
  * @author SarahHeckman
  */
 public class CourseRecordIOTest {
@@ -63,7 +62,6 @@ public class CourseRecordIOTest {
 
 	/**
 	 * Resets course_records.txt for use in other tests.
-	 * @throws Exception if the system cannot find the path specified
 	 */
 	@Before
 	public void setUp() throws Exception {
