6811b112ef76463f9123efce92759a23a79899ab
Warren Long <wclong4@ncsu.edu>
2023-11-29 19:09:57 -0500

    Removed unused imports

--- PackScheduler/test/edu/ncsu/csc216/pack_scheduler/io/CourseRecordIOTest.java
+++ PackScheduler/test/edu/ncsu/csc216/pack_scheduler/io/CourseRecordIOTest.java
@@ -15,9 +15,6 @@ import java.util.Scanner;
 
 
 import edu.ncsu.csc216.pack_scheduler.course.Course;
-import edu.ncsu.csc216.pack_scheduler.manager.RegistrationManager;
-import edu.ncsu.csc216.pack_scheduler.user.Faculty;
-import edu.ncsu.csc216.pack_scheduler.util.LinkedList;
 import edu.ncsu.csc217.collections.list.SortedList;
 
 /**
